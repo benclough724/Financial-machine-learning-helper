20a2be057ada104afb24a2171f1d044a0b9662c5
benclough724 <ben_clough123@outlook.com>
2025-02-19 20:56:16 +0000

    Project Initialisation
    just initialising the project

---
+++ venv/api.py
@@ -0,0 +1,9 @@
+
+
+# Truelayer API Credentials
+client_ID = "leeds3100"
+client_Secret = "custSecret"
+username = "b.clough2554@student.leedsbeckett.ac.uk"
+password = "hjlfF234##AS@ksa"
+
+print("1")
