e52d4b2cd292e7eced9c8ed314299b0f1c3d3562
benclough724 <ben_clough123@outlook.com>
2025-03-03 21:23:06 +0000

    Small api changes
    Small api changes. Research required to continue onto pre-processing.

--- venv/api_clients/api.py
+++ venv/api_clients/api.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import requests
 import os
 import pandas as pd
+import json
 
 load_dotenv()
 
@@ -21,7 +22,8 @@ transactions = [
         "description": t["details"]["description"],
         "date": t["details"]["completed"]
     }
-    for t in data.get("transactions", [])  # Ensure "transactions" key exists
+    for t in data.get("transactions", [])   
+    # Ensure "transactions" key exists
 ]
 
 # Convert to Pandas DataFrame
@@ -30,6 +32,8 @@ df = pd.DataFrame(transactions)
 # Convert date to datetime format
 #df["date"] = pd.to_datetime(df["date"])
 
+
+print(json.dumps(data, indent=4))
 print(df.head())  # Display first few rows
 
 
