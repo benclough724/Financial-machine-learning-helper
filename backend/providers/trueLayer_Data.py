138bc873d5b4b4cb360574710f83532dc730265d
benclough724 <ben_clough123@outlook.com>
2025-05-04 21:24:18 +0100

    main.py added

--- backend/providers/trueLayer_Data.py
+++ backend/providers/trueLayer_Data.py
@@ -1,2 +1,9 @@
 import requests
 from truelayer_auth import TrueLayerAuth 
+
+#Get the auth token from truelayer
+auth = TrueLayerAuth()
+access_token = auth.get_access_token()
+
+class TrueLayerData:
+   
