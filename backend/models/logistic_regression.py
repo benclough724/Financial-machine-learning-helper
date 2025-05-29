55fa6ae63e9d7af0eac1bc0951585fe9603cb1b0
Ben Clough <ben_clough123@outlook.com>
2025-05-29 14:22:07 +0100

    some

--- backend/models/logistic_regression.py
+++ backend/models/logistic_regression.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression 
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("tharunprabu/my-expenses-data")
+
+print("Path to dataset files:", path)
