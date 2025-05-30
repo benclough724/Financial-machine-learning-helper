aaca1ef28971580f3e835e465d761552148246c1
benclough724 <ben_clough123@outlook.com>
2025-05-30 18:41:49 +0100

    Imported kaggle dataset through kaggle api

--- backend/models/logistic_regression.py
+++ backend/models/logistic_regression.py
@@ -1,9 +1,13 @@
+import kaggle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression 
-import kagglehub
+
+
+kaggle.api.authenticate()
+
 
 # Download latest version
-path = kagglehub.dataset_download("tharunprabu/my-expenses-data")
+kaggle.api.dataset_download_files('tharunprabu/my-expenses-data', unzip=True)
 
 print("Path to dataset files:", path)
