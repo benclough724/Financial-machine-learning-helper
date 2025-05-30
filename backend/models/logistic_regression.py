92e14cf6ea2edafe87487da1d8e758a585bb0960
benclough724 <ben_clough123@outlook.com>
2025-05-31 00:09:51 +0100

    Visualization logic added

--- backend/models/logistic_regression.py
+++ backend/models/logistic_regression.py
@@ -2,12 +2,27 @@ import kaggle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression 
+import matplotlib as plt
 
 
 kaggle.api.authenticate()
 
-
-# Download latest version
+# Download kaggle dataset and unzip
 kaggle.api.dataset_download_files('tharunprabu/my-expenses-data', unzip=True)
 
-print("Path to dataset files:", path)
+df = pd.read_csv('./expense_data_1.csv')
+
+print(df.columns)
+
+
+plt.figure(figsize=(10,6))
+plt.plot(df['Date'], df['Amount'])
+plt.xlabel('Date')
+plt.ylabel('Amount')
+plt.title('Amount Over Time')
+plt.show()
+
+#print("info:::", df.info())
+#print("describe:::", df.describe())
+#print("missing values", df.isnull().sum())  # Check for missing values
+#print("Cols numbers:::", df.columns)
