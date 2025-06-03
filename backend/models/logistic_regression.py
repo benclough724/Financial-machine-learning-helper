d9956e0b95006bb110b9a75aa35a157fa63a9825
Ben Clough <ben_clough123@outlook.com>
2025-06-03 16:31:11 +0100

    New environement/ gitignore updated +requirements

--- backend/models/logistic_regression.py
+++ backend/models/logistic_regression.py
@@ -2,27 +2,25 @@ import kaggle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 kaggle.api.authenticate()
 
 # Download kaggle dataset and unzip
-kaggle.api.dataset_download_files('tharunprabu/my-expenses-data', unzip=True)
+kaggle.api.dataset_download_files('tharunprabu/my-expenses-data', path='./Datasets' ,unzip=True)
 
-df = pd.read_csv('./expense_data_1.csv')
+df = pd.read_csv('./Datasets/expense_data_1.csv')
 
 print(df.columns)
 
- 
+
 plt.figure(figsize=(10,6))
 plt.plot(df['Date'], df['Amount'])
-plt.
-plt.plot
-#plt.xlabel('Date')
-#plt.ylabel('Amount')
-#plt.title('Amount Over Time')
-#plt.show()
+plt.xlabel('Date')
+plt.ylabel('Amount')
+plt.title('Expenses Over Time')
+plt.show()
 
 #print("info:::", df.info())
 #print("describe:::", df.describe())
