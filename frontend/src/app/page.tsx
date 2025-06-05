7a29346c735276425fd3c878efa229574a369da8
benclough724 <ben_clough123@outlook.com>
2025-06-05 10:26:26 +0100

    Shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Line } from "react-chartjs-2";
-import { useEffect, useState } from "react";
+
+
 
 
 export default function DataView() {
