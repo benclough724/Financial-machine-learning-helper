113e7870324eb536c00e2b35a46a5c69f58cb92b
benclough724 <ben_clough123@outlook.com>
2025-06-05 17:22:50 +0100

    Test.ipynb added to assist with exploratory data analysis

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,59 @@
-from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI  
 from pathlib import Path
+import uvicorn
 from pipeline.kaggle import download_and_unzip_kaggle
 from pipeline.data_loader import load_data
 from pipeline.preprocess import preprocess_data
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 BASE_DIR = Path(__file__).parent  # Directory where main.py is
 DATA_DIR = BASE_DIR / "Datasets" # Dataset directory
 CSV_FILE = DATA_DIR / "expense_data_1.csv"
 
-# main pipeline for project
-def main():
-    # Download and unzip dataset if not already downloaded
-    def get_user_data_choice():
-        if not CSV_FILE.exists():
-            print(f"{CSV_FILE} not found. Downloading dataset...")
-            download_and_unzip_kaggle('tharunprabu/my-expenses-data', DATA_DIR)
-        else:
-            print(f"{CSV_FILE} found. Skipping download.")
-        
-    # Get raw data and create REST API
-    app.get('/data/raw')
-    def get_raw_data():
-        df = load_data(CSV_FILE) # load datasets
-        return df.to_dict(orient="records")
-    
-    # Get preprocessed data and create REST API
-    app.get('/data/preprocessed')
-    def get_raw_data():
-        df = load_data(CSV_FILE) # load dataset
-        df = preprocess_data(df) # preprocess dataset
-        return df.to_dict(orient="records")
 
-if __name__ == "__main__":
-    main()
+
+# main pipeline for project
+
+  # Download and unzip dataset if not already downloaded
+def get_user_data_choice():
+    if not CSV_FILE.exists():
+        print(f"{CSV_FILE} not found. Downloading dataset...")
+        download_and_unzip_kaggle('tharunprabu/my-expenses-data', DATA_DIR)
+    else:
+        print(f"{CSV_FILE} found. Skipping download.")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API!"}
+    
+# Get raw data and create REST API
+@app.get('/data/raw')
+def get_raw_data():
+    # return {"Hell: ben"} 
+    df = load_data(CSV_FILE) # load datasets
+    simple_cols = []
+    return df.to_dict(orient="records")
+    
+# Get preprocessed data and create REST API
+# 
+@app.get('/data/preprocessed')
+def get_preprocessed_data():
+    df = load_data(CSV_FILE) # load dataset
+    df = preprocess_data(df) # preprocess dataset
+    return df.head() 
+    
+    
+    
+    
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0", port=8000)
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+import { DataTable } from "@/components/ui/data-table";
 
 
+
+// Defining interface for data types of rows in db
+interface DatasetRows{
+
+}
 
 
 export default function DataView() {
-  
+  const [dataset, setDataset] = useState("");
 
   return (
     <div className="">
-
+      <DataTable />
     </div>
   );
 }
