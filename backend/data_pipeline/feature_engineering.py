035820ea3c9034e6944f351f0111420edcb95995
benclough724 <ben_clough123@outlook.com>
2025-07-01 21:33:46 +0100

    Rolling features and transaction flag

--- backend/data_pipeline/feature_engineering.py
+++ backend/data_pipeline/feature_engineering.py
@@ -3,10 +3,24 @@ import numpy as np
 
 def date_features(df, date_col='Date'):
     """Extracts useful time-based features from the date."""
-    df[date_col] = pd.to_datetime(df[date_col], errors='coerce')
-    df['DayOfWeek'] = df[date_col].dt.dayofweek
-    df['Day'] = df[date_col].dt.day
-    df['Month'] = df[date_col].dt.month
-    df['Year'] = df[date_col].dt.year
-    df['IsWeekend'] = df[date_col].dt.dayofweek >= 5
+    df[date_col] = pd.to_datetime(df[date_col], errors='coerce') # Date column --> datetime object
+    df['DayOfWeek'] = df[date_col].dt.dayofweek # extract the day of the week
+    df['Day'] = df[date_col].dt.day # extract day 
+    df['Month'] = df[date_col].dt.month # extract month
+    df['Year'] = df[date_col].dt.year # extract year 
+    df['IsWeekend'] = df[date_col].dt.dayofweek >= 5 # adds boolean if transaction happened on a weekend
+    return df
+
+
+def rolling_features(df, amount_col='Amount', date_col='Date'):
+    """Adds rolling average/spending features based on amount."""
+    df = df.sort_values(by=date_col)
+    df['RollingMean_3'] = df[amount_col].rolling(window=3, min_periods=1).mean()
+    df['RollingStd_3'] = df[amount_col].rolling(window=3, min_periods=1).std().fillna(0)
+    df['CumulativeSum'] = df[amount_col].cumsum()
+    return df
+
+def add_transaction_type_flag(df, type_col='TransactionType'):
+    """Converts 'Income' / 'Expense' to binary flag."""
+    df['IsIncome'] = df[type_col].str.lower().map({'income': 1, 'expense': 0})
     return df
